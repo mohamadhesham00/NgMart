3bc83bd6657952e52c677e1acc6aa08a84ce1c82
mohamadhesham00 <mohamadhesham112@gmail.com>
2025-09-20 13:50:46 +0300

    Build: added configs for tailwind & primeng & other packages

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,31 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
-
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Noir from './themes/MyPreset';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideRouter(routes),
+    provideHttpClient(withFetch()),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      // theme: { preset: Aura, options: { darkModeSelector: '.p-dark' } },
+      theme: {
+        preset: Noir,
+        options: {
+          // Tell PrimeNG to switch to dark mode when the 'dark-theme' class is on the body
+          darkModeSelector: '.dark-theme',
+        },
+      },
+    }),
+
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
+  ],
 };

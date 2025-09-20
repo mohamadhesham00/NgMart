bab8db249f39fe7704ef7d0cc935c44dc631162c
mohamadhesham00 <mohamadhesham112@gmail.com>
2025-09-20 14:14:56 +0300

    Feat: added initial routing for different components with guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { Cart } from './components/cart/cart';
+import { Home } from './components/home/home';
+import { Login } from './components/login/login';
+import { ProductDetails } from './components/product-details/product-details';
+import { Products } from './components/products/products';
+import { Profile } from './components/profile/profile';
+import { Wishlist } from './components/wishlist/wishlist';
+import { authGuard } from './guards/auth-guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home, pathMatch: 'full' },
+  { path: 'home', component: Home },
+  { path: 'products', component: Products },
+  { path: 'product/:id', component: ProductDetails },
+  { path: 'login', component: Login },
+  { path: 'cart', component: Cart, canActivate: [authGuard] },
+  { path: 'profile', component: Profile },
+  { path: 'wishlist', component: Wishlist, canActivate: [authGuard] },
+];

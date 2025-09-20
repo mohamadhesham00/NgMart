6a7f1d6259de02e5803e26f37f468c194d28dc53
mohamadhesham00 <mohamadhesham112@gmail.com>
2025-09-20 14:13:10 +0300

    Feat: updated app.html to load needed components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,15 @@
-import { Component, signal } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, signal } from '@angular/core';
+import { Header } from './components/header/header';
+import { ProductCarousel } from './components/product-carousel/product-carousel';
 import { RouterOutlet } from '@angular/router';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Header, Footer, RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class App {
   protected readonly title = signal('NgMart');
